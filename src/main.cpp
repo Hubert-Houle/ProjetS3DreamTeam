6ed35e91baaf3de527a5f156ddb23ff26b5f6865
DEEEEEEEEEZ <beaudoin.ethan@gmail.com>
2023-07-15 14:31:59 -0400

    fleur

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,8 @@ DenisCodeur = new encodeur;
 
   while(1)
   {
+  if(DenisCodeur->getPosition()>0.05){
+  while(DenisCodeur->getPosition()>0.05 || DenisCodeur->getPosition()<1.6){
     PID_absorbtion(BNO, 0.15, 0.0001, 0);
     //PID_absorbtion(BNO, -0.03, -0.0001, -0.0001);
 
