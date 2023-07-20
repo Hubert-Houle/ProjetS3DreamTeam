9a2bd032aebb7416e23a0a45fad07e11c780a623
Hubert-Houle <Houh1903@usherbrooke.ca>
2023-07-20 13:47:53 -0400

    meca 2.0

--- src/LibDreamT.h
+++ src/LibDreamT.h
@@ -28,6 +28,9 @@
 #define MagnetOn        1
 #define MagnetOff       0
 
+#define POSITION_FIN_RAIL 1.5
+#define POSITION_DEBUT_RAIL 0
+#define GAIN_BARRIERE_VIRTUELLE -0.1
 //---------------------------- variables globales ---------------------------
 ArduinoX AX_;                       // objet arduinoX
 MegaServo servo_;                   // objet servomoteur
@@ -54,6 +57,7 @@ float Mxyz[3];                      // tableau pour magnetometre
 
 // MEF
 int Etat;
+
 
 //Adafruit_BNO055 bno = Adafruit_BNO055(55);
 //imu::Vector<3> AnglesPendule;
@@ -289,8 +293,26 @@ void Magnet(bool StateMagnet)
 
 
 
-//--  Motor -------------------------------------
+//--  PID_lineaire -------------------------------------
+ bool fct_PID_position(encodeur* enCodeur, DT_pid* pid1,DT_pid* pid2){
 
+  float CV;
+  float Barriere_Virtuelle=GAIN_BARRIERE_VIRTUELLE * (1/((POSITION_FIN_RAIL-enCodeur->getPosition())*(enCodeur->getVitesse()>0))+((POSITION_DEBUT_RAIL-enCodeur->getPosition())*(enCodeur->getVitesse()<0)));
+  //pid2->setSP(Barriere_Virtuelle);
+
+  
+
+  //pid1->setSP(CV);
+	CV=pid1->response_Sum();
+  CV+=Barriere_Virtuelle;
+
+  
+	AX_.setMotorPWM(0, (CV>1) ? 1: ((CV<-1) ? -1 : CV));  // retourner CV si entre 1 et -1 sinon retourner 1 ou -1
+	
+	return 1;
+
+ 	
+ }
 //--  Motor -------------------------------------
 //  tableau[0] = previus Time
 //  tableau[1] = previus pulse
